974661a663c071a83532f3dc53ba87d18f11a89e
Lisa Li <lisali@172-30-22-4.DYNAPOOL.NYU.EDU>
2018-03-23 23:14:33 -0400

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express()
 
 app.get('/', function(req, res) {res.render('index')})
 
+app.get('/user', function(req, res) {res.render('login')})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 

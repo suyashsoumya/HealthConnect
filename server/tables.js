996a6803eed550bed8c0064d429af82f0e4ecaba
Vanessa Chu <vanessa.chu@outlook.com>
2018-08-14 16:01:39 -0400

    more options for DB software

--- server/tables.js
+++ server/tables.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 var sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
     define: {
         timestamps: false
     }
